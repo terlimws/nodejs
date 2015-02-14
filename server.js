a92c52fb67116efcdde808b0264ee475a473d497
iamsq <iamsq>
2015-02-14 17:27:53 +0000

    hello world commit

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('Hello World \n');
+  res.end('I\'m  not a noob boy ok =)\n');
 }).listen(port);

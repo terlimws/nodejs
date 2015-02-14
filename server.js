b3b94a9c279e401c418033509f65c7ffbe726a0e
iamsq <shi.ng.10@ucl.ac.uk>
2015-02-15 01:31:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World \n');
-  res.end('I\'m  not a noob boy ok =)\n');
+  res.end('Hello World <br> I\'m  not a noob boy ok =)\n');
 }).listen(port);
